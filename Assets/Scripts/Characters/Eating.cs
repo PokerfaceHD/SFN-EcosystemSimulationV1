c14215196a4fdcb284ebdd81776145c617d2cb6b
Jimmy <52115196+jimmyy341@users.noreply.github.com>
2020-12-08 11:07:33 +0100

    Added Assets
    Characters is already able to eat, move and replicate (plant eaters).

---
+++ Assets/Scripts/Characters/Eating.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Eating : MonoBehaviour
+{
+    float hunger = 100;
+    public bool meat;
+    public bool plant;
+    GameObject[] gos;
+    GameObject target;
+    public float foodFindingCooldown = 1;
+    float timeToNextFoodFind;
+    public float speed = 2;
+    public float digestionEfficiencyMeat;
+    public float digestionEfficiencyPlant;
+    Vector3 moveDirection;
+    Vector3 diff;
+    GameObject closest;
+    Vector3 closestDirection;
+    public Rigidbody rb;
+    public float detectionRange = 30;
+    void Update()
+    {
+        if (hunger > 100)
+        {
+            hunger = 100;
+            Instantiate(this, transform.position + new Vector3(Random.Range(-1,1), 0, Random.Range(-1, 1)), new Quaternion(0,0,0,0));
+        }
+        if (closestDirection.magnitude > detectionRange)
+        {
+            moveDirection = (closest.transform.position - transform.position).normalized;
+        }
+        else
+        {
+            Vector3 randomDirection = new Vector3(Random.Range(-100f, 100f), 0f, Random.Range(-100f, 100f));
+            moveDirection = randomDirection.normalized;
+        }
+        if (timeToNextFoodFind < Time.time)
+        {
+            timeToNextFoodFind = Time.time + foodFindingCooldown;
+            if (hunger > 0)
+            {
+
+                if (meat)
+                {
+                    gos = GameObject.FindGameObjectsWithTag("Meat");
+                }
+                if (plant)
+                {
+                    gos = GameObject.FindGameObjectsWithTag("Plant");
+                }
+                closest = null;
+                float distance = Mathf.Infinity;
+                Vector3 position = transform.position;
+                foreach (GameObject go in gos)
+                {
+                    diff = go.transform.position - position;
+                    float curDistance = diff.sqrMagnitude;
+                    if (curDistance < distance)
+                    {
+                        closest = go;
+                        distance = curDistance;
+                    }
+                }
+                closestDirection = closest.transform.position - transform.position;
+                if ((closest.transform.position - transform.position).magnitude > detectionRange) 
+                {
+                    Vector3 randomDirection = new Vector3(Random.Range(-100f, 100f), 0f, Random.Range(-100f, 100f));
+                    moveDirection = randomDirection.normalized;
+                    timeToNextFoodFind = Time.time + foodFindingCooldown * 5;
+                }
+                else
+                {
+                    moveDirection = (closest.transform.position - transform.position).normalized;
+                }
+            }
+        }
+        rb.velocity = moveDirection * speed;
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (meat)
+        {
+            if(collision.collider.tag == "Meat")
+            {
+                hunger += 25 * digestionEfficiencyMeat;
+                Destroy(closest);
+            }
+        }
+        if (plant)
+        {
+            if (collision.collider.tag == "Plant")
+            {
+                hunger += 15 * digestionEfficiencyPlant;
+                Destroy(collision.collider.gameObject);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Environment/Test.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour {
+
+	public float radius = 1;
+	public Vector2 regionSize = Vector2.one;
+	public int rejectionSamples = 30;
+	public float displayRadius =1;
+    public GameObject food;
+    Vector3 point3D;
+	List<Vector2> points;
+
+	void OnValidate() {
+		points = PoissonDiscSampling.GeneratePoints(radius, regionSize, rejectionSamples);
+	}
+
+	void Start() {
+
+		if (points != null) {
+			foreach (Vector2 point in points) {
+                point3D.x = point.x - regionSize.x/2;
+                point3D.z = point.y - regionSize.y/2;
+                Instantiate(food, point3D, new Quaternion(0,0,0,0));
+				print("lul");
+			}
+		}
+	}
+}
